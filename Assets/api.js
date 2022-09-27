3cc0b66a7feed1dffda67a4012c12b00435697e3
DMP <96370195+MilkyProject@users.noreply.github.com>
2022-09-27 18:29:37 +0300

    Update api.js

--- Assets/api.js
+++ Assets/api.js
@@ -6,7 +6,7 @@ const options = {
 	},
 };
 
-fetch('https://api.scpslgame.com/serverinfo.php?id=21611&key=ENeppKLJhYlqjjGllyxPaDNa&players=true', options)
+fetch('https://cors-anywhere.herokuapp.com/corsdemo/https://api.scpslgame.com/serverinfo.php?id=21611&key=ENeppKLJhYlqjjGllyxPaDNa&players=true', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
